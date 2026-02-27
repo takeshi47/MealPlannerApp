aaed3cf02a522d34db743a501d70b4aa7cbeb85c
takeshi47 <letsmakeawish47@gmail.com>
2026-02-27 21:33:44 +0900

    feat: redesign menu list UI and integrate modal-based form

--- frontend/src/app/components/menu/menu-list/menu-list.ts
+++ frontend/src/app/components/menu/menu-list/menu-list.ts
@@ -1,21 +1,58 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MenuService } from '../../../services/menu-service';
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable } from 'rxjs';
 import { Menu } from '../../../models/menu';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap/modal';
+import { MenuForm } from '../menu-form/menu-form';
 
 @Component({
   selector: 'app-menu-list',
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './menu-list.html',
   styleUrl: './menu-list.scss',
 })
 export class MenuList implements OnInit {
   private menuService = inject(MenuService);
+  private modalService = inject(NgbModal);
   protected menus$!: Observable<Menu[]>;
 
   ngOnInit(): void {
-    this.menus$ = this.menuService.fetchAll();
+    this.loadMenus();
+  }
+
+  private loadMenus(): void {
+    this.menus$ = this.menuService.fetchAll().pipe(
+      catchError((err) => {
+        console.error(err);
+        return EMPTY;
+      }),
+    );
+  }
+
+  protected onDelete(id: number): void {
+    if (!confirm('本当に削除して大丈夫？')) {
+      return;
+    }
+
+    // todo: 削除処理
+    console.log(id);
+  }
+
+  protected openMenuForm(id: number | null = null): void {
+    const modalRef = this.modalService.open(MenuForm, {
+      size: 'mg',
+    });
+
+    modalRef.componentInstance.id = id;
+
+    modalRef.result.then(
+      () => {
+        this.loadMenus();
+      },
+      (reject) => {
+        console.log(reject);
+      },
+    );
   }
 }
