98f25cf87df1777a6aaa945b1a79039c58d71f4c
takeshi47 <letsmakeawish47@gmail.com>
2026-03-01 09:28:54 +0900

    test: php bin/console make:test TestCase Repository\\IngredientRepositoryTest

---
+++ tests/Repository/IngredientRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repository;
+
+use PHPUnit\Framework\TestCase;
+
+class IngredientRepositoryTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
